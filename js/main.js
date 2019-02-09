5e9678e0169e4a35b5e4508d8fd06dc465419dea
Francuski Miroslav <francuski.miroslav@gmail.com>
2019-02-09 03:11:02 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-  $(".menu-btn").click(function() {
-    if (!$(this).hasClass("close")) {
-      $(".menu-btn").addClass("close");
-      $(".menu, .menu-nav, .menu-branding, .nav-item").addClass("show");
-    } else {
-      $(".menu-btn").removeClass("close");
-      $(".menu, .menu-nav, .menu-branding, .nav-item").removeClass("show");
-    }
-  });
-});
+$(document).ready(function(){$(".menu-btn").click(function(){$(this).hasClass("close")?($(".menu-btn").removeClass("close"),$(".menu, .menu-nav, .menu-branding, .nav-item").removeClass("show")):($(".menu-btn").addClass("close"),$(".menu, .menu-nav, .menu-branding, .nav-item").addClass("show"))})});
