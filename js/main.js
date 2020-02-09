44ba7170a7b88a97f7305dbec08a791e222ec297
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-02-09 16:17:55 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
-$(document).ready(function(){$(".menu-btn").click(function(){$(this).hasClass("close")?($(".menu-btn").removeClass("close"),$(".menu, .menu-nav, .menu-branding, .nav-item").removeClass("show")):($(".menu-btn").addClass("close"),$(".menu, .menu-nav, .menu-branding, .nav-item").addClass("show"))})});
+/*! project-name v0.0.1 | (c) 2020 Francuski Miroslav | MIT License | http://link-to-your-git-repo.com */
+// navigation
+$(document).ready((function() {
+  $(".menu-btn").click((function() {
+    if (!$(this).hasClass("close")) {
+      $(".menu-btn").addClass("close");
+      $(".menu, .menu-nav, .menu-branding, .nav-item").addClass("show");
+    } else {
+      $(".menu-btn").removeClass("close");
+      $(".menu, .menu-nav, .menu-branding, .nav-item").removeClass("show");
+    }
+  }));
+}));
+
+// isotope initiate
+var $grid = $(".projects").isotope({
+  itemSelector: ".item",
+  layoutMode: "fitRows"
+});
+
+var filters = {};
+
+$(".portfolio-menu").on("change", (function(event) {
+  var $select = $(event.target);
+  // get group key
+  var filterGroup = $select.attr("value-group");
+  // set filter for group
+  filters[filterGroup] = event.target.value;
+  // combine filters
+  var filterValue = concatValues(filters);
+  // set filter for Isotope
+  $grid.isotope({ filter: filterValue });
+}));
+
+// flatten object by concatting values
+function concatValues(obj) {
+  var value = "";
+  for (var prop in obj) {
+    value += obj[prop];
+  }
+  return value;
+}
+
+// success message page - go back button
+function goBack() {
+  window.history.back();
+}
