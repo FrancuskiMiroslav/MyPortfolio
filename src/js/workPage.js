9e109e4b9bdeda188b8bc5e6f72ec6356dc0cc3d
Francuski Miroslav <francuski.miroslav@gmail.com>
2021-02-19 11:52:45 +0100

    updated work page with fetch data from json file

--- src/js/workPage.js
+++ src/js/workPage.js
@@ -1,32 +1,34 @@
-// isotope initiate
-const work = document.getElementById('work');
+document.addEventListener('DOMContentLoaded', function () {
+	// isotope initiate
+	const work = document.getElementById('work');
 
-if (work) {
-	let $grid = $('.projects').isotope({
-		itemSelector: '.item',
-		layoutMode: 'fitRows',
-	});
+	if (work) {
+		let $grid = $('.projects').isotope({
+			itemSelector: '.item',
+			layoutMode: 'fitRows',
+		});
 
-	let filters = {};
+		let filters = {};
 
-	$('.portfolio-menu').on('change', function (event) {
-		var $select = $(event.target);
-		// get group key
-		var filterGroup = $select.attr('value-group');
-		// set filter for group
-		filters[filterGroup] = event.target.value;
-		// combine filters
-		var filterValue = concatValues(filters);
-		// set filter for Isotope
-		$grid.isotope({ filter: filterValue });
-	});
+		$('.portfolio-menu').on('change', function (event) {
+			var $select = $(event.target);
+			// get group key
+			var filterGroup = $select.attr('value-group');
+			// set filter for group
+			filters[filterGroup] = event.target.value;
+			// combine filters
+			var filterValue = concatValues(filters);
+			// set filter for Isotope
+			$grid.isotope({ filter: filterValue });
+		});
 
-	// flatten object by concatting values
-	function concatValues(obj) {
-		var value = '';
-		for (var prop in obj) {
-			value += obj[prop];
+		// flatten object by concatting values
+		function concatValues(obj) {
+			var value = '';
+			for (var prop in obj) {
+				value += obj[prop];
+			}
+			return value;
 		}
-		return value;
 	}
-}
+});
